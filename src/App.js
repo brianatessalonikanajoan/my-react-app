bd1eb08ac6febb12939357ee9b89e29dcf5a7f28
brianatessalonikanajoan <s22210456@student.unklab.ac.id>
2024-06-25 17:07:08 -0800

    initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const hello ="Hello World!!!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //jsx
+    <div>
+      {/* komen dalam jsx */}
+      <h2>{hello}</h2>
+      <h4> Briana Tessalonika Najoan S.kom, M.kom, P.hd</h4>
     </div>
   );
-}
+};
 
 export default App;
+
+//komponen adalah sebuah fungsi yang mereturn jsx
+//div disebut elemen
+//kalo mau tmapilkan variable dalam jsx harus pake{}
+//dalam satu file hanya boleh 1 komponen
---
+++ src/Greetings.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Greetings = () => {
+    return <h3>ini adalah komponen greetings2</h3>
+  }; 
+  export default Greetings;
